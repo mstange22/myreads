539b7f0f07f98388775d355644da06063fa92ac5
Michael Stange <mstange22@yahoo.com>
2017-11-17 22:23:14 -0800

    wait for promise before re-rendering view

--- src/Bookshelves.js
+++ src/Bookshelves.js
@@ -8,29 +8,30 @@ import Read from './Read'
 class Bookshelves extends Component {
   
   state = {
-    booksCurrent: [],
-    booksWant: [],
-    booksRead: []
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
   }
 
+  // get all books on page load
   componentDidMount() {
+    this.getAll()
+  }
+
+  // load all books and split into appropriate array, re-render page
+  getAll = () => {
     BooksAPI.getAll().then(books => {
       this.setState({
-        booksCurrent: books.filter(book => book.shelf === "currentlyReading"),
-        booksWant: books.filter(book => book.shelf === "wantToRead"),
-        booksRead: books.filter(book => book.shelf === "read")
+        currentlyReading: books.filter(book => book.shelf === "currentlyReading"),
+        wantToRead: books.filter(book => book.shelf === "wantToRead"),
+        read: books.filter(book => book.shelf === "read")
       })
     })
   }
 
+  // update book with new shelf and reload books
   handleShelfChange = (book, shelf) => {
-		BooksAPI.update(book, shelf).then(BooksAPI.getAll().then(books => {
-      this.setState({
-        booksCurrent: books.filter(book => book.shelf === "currentlyReading"),
-        booksWant: books.filter(book => book.shelf === "wantToRead"),
-        booksRead: books.filter(book => book.shelf === "read")
-      })
-    }))
+    BooksAPI.update(book, shelf).then(() => this.getAll())
 	}
 
   render() {
@@ -40,9 +41,10 @@ class Bookshelves extends Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <CurrentlyReading booksCurrent={this.state.booksCurrent} handleShelfChange={this.handleShelfChange}/>
-          <WantToRead booksWant={this.state.booksWant} handleShelfChange={this.handleShelfChange}/>
-          <Read booksRead={this.state.booksRead} handleShelfChange={this.handleShelfChange}/>
+          {/* insert shelf components - pass array of books and handleShelfChange function */}
+          <CurrentlyReading currentlyReading={this.state.currentlyReading} handleShelfChange={this.handleShelfChange}/>
+          <WantToRead wantToRead={this.state.wantToRead} handleShelfChange={this.handleShelfChange}/>
+          <Read read={this.state.read} handleShelfChange={this.handleShelfChange}/>
         </div>
         <div className="open-search">
           <Link to='/search'>Add a book</Link>
