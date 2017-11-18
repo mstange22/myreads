a891f7581cc143eb86950cc26fe76ff8f2c6b54c
Michael Stange <mstange22@yahoo.com>
2017-11-17 22:23:26 -0800

    comments & formatting

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import ShelfChanger from './ShelfChanger'
+import PropTypes from 'prop-types'
 
 class Book extends Component {
+    static propTypes = {
+        handleShelfChange: PropTypes.func.isRequired
+    }
+
 	render() {
 		let book = this.props.book
 		return (
 			<div className="book">
 				<div className="book-top">
+					{/* handle cases where there are no image links */}
 					{book.imageLinks ? (
 						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
 					) : (
@@ -15,7 +21,9 @@ class Book extends Component {
 					<ShelfChanger book={book}  handleShelfChange={this.props.handleShelfChange}/>
 				</div>
 				<div className="book-title">{book.title}</div>
+				{/* handle cases where there are no authors */}
 				{book.authors ? (
+					// in the event of multiple authors, list them out
 					book.authors.map(author => (
 						<div key={author} className="book-authors">{author}</div>
 					))
